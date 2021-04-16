9886cf3b3cabe3cf642765260d1d924de85f9dc2
Geoff Hom <geoff.hom@gmail.com>
2021-04-15 20:05:19 -0500

    Start in SwiftUI
    I started the Xcode project as "Game" rather than "App." However, we
    want the latter, as the app will be in SwiftUI, with tests, and the
    game will be shown via SpriteView. So, add SwiftUI so that the project
    is as if we started with "App," including tests. In a future commit,
    add SpriteKit via SpriteView.
    
    Also, I'm trying to get experience with TDD. So, add first test. (Does
    "Hello, world!" exist?)

---
+++ Xcode/RePassage Shared/RePassageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RePassageApp.swift
+//  RePassage
+//
+//  Created by Geoff Hom on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RePassageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Xcode/RePassage macOS/AppDelegate.swift
+++ Xcode/RePassage macOS/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-@main
+//@main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
