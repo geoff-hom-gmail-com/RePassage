94f50931dd68df1248f5f80ce2c00d5ab25bce17
Geoff Hom <geoff.hom@gmail.com>
2021-04-22 22:10:09 -0500

    Wrap lines at 100 characters
    Airbnb guideline. Google is similar.

--- Xcode/RePassageUITests/RePassageUITests.swift
+++ Xcode/RePassageUITests/RePassageUITests.swift
@@ -14,16 +14,19 @@ public enum Texts: String {
 class RePassageUITests: XCTestCase {
 
   override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Put setup code here. This method is called before the invocation of each test method in the
+    // class.
 
     // In UI tests it is usually best to stop immediately when a failure occurs.
     continueAfterFailure = false
 
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    // In UI tests it’s important to set the initial state - such as interface orientation -
+    // required for your tests before they run. The setUp method is a good place to do this.
   }
 
   override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // Put teardown code here. This method is called after the invocation of each test method in the
+    // class.
   }
 
   func testExample() throws {
@@ -38,7 +41,8 @@ class RePassageUITests: XCTestCase {
   func testHelloWorldText() throws {
     let app = XCUIApplication()
     app.launch()
-    // TODO: consider moving app launch to setup? except testLaunchPerformance doesn't use that? look up the measure() function and XCTApplicationLaunchMetric()
+    // TODO: consider moving app launch to setup? except testLaunchPerformance doesn't use that?
+    // look up the measure() function and XCTApplicationLaunchMetric()
     XCTAssertTrue(app.staticTexts[Texts.helloWorld.rawValue].exists)
   }
 
